8c2a354b68b1d9f029ed5db5a44fb2e2f8e339cd
Michel Michels <michel.michels.jr@gmail.com>
2019-08-12 15:58:26 +0200

    Initial commit

---
+++ CliSharp/CliSharp/Attributes/ParameterName.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CliSharp.Attributes
+{
+    public class ParameterName : Attribute
+    {
+        public ParameterName(string name) : base()
+        {
+            Name = name;
+        }
+
+        public string Name { get; private set; }
+    }
+}
